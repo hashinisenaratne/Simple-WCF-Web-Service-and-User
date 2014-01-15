4098142c0949884fada9f9f43fee005b259f44c4
Hashini Senaratne <hashz1990@gmail.com>
2014-01-15 18:42:26 +0530

    Basic service and accessor

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            Console.WriteLine("8 + 4 = {0}",client.Add(8,4));
+            Console.ReadLine();
+        }
+    }
+}
